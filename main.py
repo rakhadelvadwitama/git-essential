c259a5e275702606c432d1aba68b9f960f47f78d
rakha delva dwi tama aji <rakha.5190411270@student.uty.ac.id>
2025-06-30 11:33:22 +0700

    added hello world 4times

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Hello, World!")
+
+print("This is a Python script running in a Docker container.")
+print("You can modify this script to suit your needs.")
+print("Make sure to rebuild the Docker image after making changes to this script.")
