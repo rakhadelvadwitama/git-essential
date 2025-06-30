a7a7cfb50671688a7c5d806ff20918267966d3db
rakha delva dwi tama aji <rakha.5190411270@student.uty.ac.id>
2025-06-30 11:37:02 +0700

    adding index.js

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
+print("Hello, World!")
 
-print("This is a Python script running in a Docker container.")
-print("You can modify this script to suit your needs.")
-print("Make sure to rebuild the Docker image after making changes to this script.")
+print("Hello, World!")
